d119095caa252f86c1cc8c0073c40430c8e5c12d
MehdiSpring <mehdi.boumzi@gmail.com>
2022-12-26 11:09:36 +0100

    Add Lombook constructor annotations

--- src/main/java/com/mbo/perfumery/model/Parfum.java
+++ src/main/java/com/mbo/perfumery/model/Parfum.java
@@ -1,13 +1,17 @@
 package com.mbo.perfumery.model;
 
 import com.mbo.perfumery.enums.Category;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Parfum {
 
