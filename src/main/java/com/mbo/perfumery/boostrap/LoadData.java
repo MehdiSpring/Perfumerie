08ce4dfdcca81f128a7e64460fcbed32b6a176da
MehdiSpring <mehdi.boumzi@gmail.com>
2023-01-05 16:41:49 +0100

    Disable loadData in boot

--- src/main/java/com/mbo/perfumery/boostrap/LoadData.java
+++ src/main/java/com/mbo/perfumery/boostrap/LoadData.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class LoadData implements CommandLineRunner {
 
