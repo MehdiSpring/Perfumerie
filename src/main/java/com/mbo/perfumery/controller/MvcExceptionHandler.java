7e8c195fc41f9af70ebcb01798d88a5e4a9b266a
MehdiSpring <mehdi.boumzi@gmail.com>
2023-01-08 10:08:10 +0100

    Issue #16. A controller advice has been added to handle MethodArgumentNotValidException. Closes #16

---
+++ src/main/java/com/mbo/perfumery/controller/MvcExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.mbo.perfumery.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> validationErrorHandler(MethodArgumentNotValidException ex){
+
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
