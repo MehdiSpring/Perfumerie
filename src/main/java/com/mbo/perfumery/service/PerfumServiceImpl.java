8a09470247754c2c5e193712dc8f69e75f67dc88
MehdiSpring <mehdi.boumzi@gmail.com>
2023-10-23 11:41:28 +0200

    Implementing The new method

--- src/main/java/com/mbo/perfumery/service/PerfumServiceImpl.java
+++ src/main/java/com/mbo/perfumery/service/PerfumServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+//Implementing The new method
 @Service
 @RequiredArgsConstructor
 public class PerfumServiceImpl implements PerfumService {
